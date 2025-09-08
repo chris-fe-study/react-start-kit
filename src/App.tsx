73bce426215c98e11fa0481b91aa32ec00cc140e
hov-i <dbsghdql55555@gmail.com>
2025-09-08 18:01:26 +0900

    fix: main.tsx router 설정 및 router 폴더 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from "react-router";
-import { router } from "@/router";
+import { router } from "@/app/routers/router";
 
 function App() {
   return <RouterProvider router={router} />;
